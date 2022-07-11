0f27d258cb4cea042baf982774981526864b6fb1
hanglong4000 <sapbatcheung@gmail.com>
2022-07-11 12:02:46 +0700

    api

--- pages/eom.js
+++ pages/eom.js
@@ -1,8 +1,31 @@
-export const EOM = () => {
-return (
-    <div> The Light is far, far away, in a place that no Communists can reach </div>
-);
+import Head from "next/head";
+import styles from "../styles/EOM.module.css";
+import { Toolbar } from "../components/toolbar";
 
-}
+export const EOM = ({ employee }) => {
+  console.log(employee);
+  return (
+    <>
+      <div className="page-container">
+        <div>
+          <h1>Employee of the month</h1>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default EOM; 
+export const getServerSideProps = async (pageContext) => {
+  const apiResponse = await fetch(
+    "https://my-json-server.typicode.com/high400/news-app/employeeOfTheMonth"
+  );
+  const employee = await apiResponse.json();
+
+  return {
+    props: {
+      employee,
+    },
+  };
+};
+
+export default EOM;
