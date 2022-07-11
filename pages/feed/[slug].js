9b216f6b71d452f36e8d3d7b305a060ead9be3a5
hanglong4000 <sapbatcheung@gmail.com>
2022-07-11 23:05:15 +0700

    changed to Singapore

--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -86,7 +86,7 @@ export const getServerSideProps = async pageContext => {
   }
 
   const apiResponse = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=tw&pageSize=5&page=${pageNumber}`,
+    `https://newsapi.org/v2/top-headlines?country=sg&pageSize=5&page=${pageNumber}`,
     {
       headers: {
         Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
