530e823f5f86ef08e301ed8e7923274f4626a4b8
hanglong4000 <sapbatcheung@gmail.com>
2022-07-20 18:19:40 +0700

    target blank

--- components/facebook.js
+++ components/facebook.js
@@ -2,12 +2,10 @@ import React from "react";
 
 export default function facebook() {
   return (
-    <div
-      onClick={() =>
-        (window.location.href = "https://www.facebook.com/lehung6000")
-      }
-    >
-      Facebook
+    <div>
+      <a target="_blank" href="https://www.facebook.com/lehung6000">
+        Facebook
+      </a>
     </div>
   );
 }
